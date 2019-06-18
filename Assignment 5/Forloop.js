edea5fa2fbd77adcf6ff18dcef545aea9941438d
Akashbangera7 <35471920+Akashbangera7@users.noreply.github.com>
2019-06-18 13:03:35 -0400

    Assignment 5&6

---
+++ Assignment 5/Forloop.js
@@ -0,0 +1,28 @@
+for(var i = -10; i <= 19; i++)
+{
+	console.log(i);
+}
+
+for(var even = 10; even <= 40; even++)
+{
+	if(even % 2 === 0)
+	{
+		console.log(even);
+	}
+}
+
+for(var odd = 300; odd <= 333; odd++)
+{
+	if(odd % 2 !== 0)
+	{
+		console.log(odd);
+	}
+}
+
+for(div = 5; div <= 50; div++)
+{
+	if(div % 5 === 0 && div % 3 === 0)
+	{
+		console.log(div);
+	}
+}
