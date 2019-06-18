6bcddd5826e3d8c174d0a9ca32cb8b1f9624986e
Akashbangera7 <35471920+Akashbangera7@users.noreply.github.com>
2019-06-18 13:01:11 -0400

    Assignment 2

---
+++ Assignment 2/Stalker.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+console.log("Full name is " +firstName+""+lastName);
+console.log("Age is " +age);
