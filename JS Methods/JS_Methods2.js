463ddad16dfc3d1858f7f11b29482d27ee090ff1
Akashbangera7 <35471920+Akashbangera7@users.noreply.github.com>
2019-06-18 13:06:42 -0400

    methods

---
+++ JS Methods/JS_Methods2.js
@@ -0,0 +1,11 @@
+var first = document.getElementById("first");
+console.log("first");
+
+var second = document.querySelector("#first");
+console.log(second);
+
+var third = document.querySelector(".special");
+console.log(third);
+
+var fourth = document.getElementsByClassName("special")[0];
+console.log(fourth);
---
+++ JS Methods/JS_methods.js
@@ -0,0 +1,29 @@
+var id = document.getElementById("highlight");
+console.log(id);
+
+
+var tags = document.getElementsByClassName("bolded");
+console.log(tags);
+
+
+var tagNames = document.getElementsByTagName("li");
+console.log(tagNames);
+
+
+var tagName = document.getElementsByTagName("li");
+console.log(tagName[0]);
+
+
+var query = document.querySelector("#highlight");
+console.log(query);
+
+
+var query2 = document.querySelector(".bolded");
+console.log(query2);
+
+
+var query3 = document.querySelectorAll("li");
+console.log(query3);
+
+var query4 = document.querySelectorAll(".bolded");
+console.log(query4);
